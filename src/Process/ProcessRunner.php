48417011f371dc27deded1786ca9cc8041e4e3b7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-01-07 21:09:31 -0600

    Create ProcessRunner.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Process/ProcessRunner.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\ComposerLocker\Process;
+
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Process;
+
+final class ProcessRunner
+{
+    public function __construct(
+        private SymfonyStyle $symfonyStyle
+    ) {
+    }
+
+    public function run(array $command, ?string $cwd = null): string
+    {
+        $this->symfonyStyle->note(
+            sprintf('Running command: "%s"', implode(' ', $command))
+        );
+
+        return $this->createProcess($command, $cwd ?? getcwd())
+            ->mustRun()
+            ->getOutput();
+    }
+
+    private function createProcess(array $command, string $cwd): Process
+    {
+        return new Process($command, $cwd);
+    }
+}
