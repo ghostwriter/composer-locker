65ca6213a22d6530cc25b4d44036a48a85f5af7b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-01-08 16:36:00 -0600

    Update Git.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Value/Git.php
+++ src/Value/Git.php
@@ -6,9 +6,9 @@ namespace Ghostwriter\ComposerLocker\Value;
 
 final class Git
 {
-    public const CHECKOUT_BRANCH = ['git', 'switch', self::HEAD_BRANCH];
+    public const ADD_ALL = ['git', 'add', '--all'];
 
-    public const CHECKOUT_NEW_BRANCH = ['git', 'switch', '-c', self::HEAD_BRANCH];
+    public const CHECKOUT_NEW_BRANCH = ['git', 'checkout', '-B', self::FEATURE_BRANCH, self::TARGET_BRANCH];
 
     public const COMMIT = [
         'git',
@@ -23,7 +23,7 @@ final class Git
 
     public const COMMIT_MESSAGE = 'Bump composer dependencies';
 
-    public const HEAD_BRANCH = 'chore/composer-locker';
+    public const FEATURE_BRANCH = 'chore/composer-locker';
 
     public const TARGET_BRANCH = 'main';
 }
