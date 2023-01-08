51f4e759afc8bfb0c62c56d16f43f95a3a5de512
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-01-07 21:25:32 -0600

    Create Lock.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Event/Lock.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\ComposerLocker\Event;
+
+use Ghostwriter\EventDispatcher\Contract\EventInterface;
+use Ghostwriter\EventDispatcher\Traits\EventTrait;
+
+final class Lock implements EventInterface
+{
+    use EventTrait;
+
+    public function __construct(
+        private readonly string $branch,
+        private readonly string $cwd,
+    ) {
+    }
+
+    public function getBranch(): string
+    {
+        return $this->branch;
+    }
+
+    public function getCurrentWorkingDirectory(): string
+    {
+        return $this->cwd;
+    }
+}
