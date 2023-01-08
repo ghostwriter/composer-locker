f7c0ffda28bc61b3d277339f2946d6e9ecdad03f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-01-08 16:24:42 -0600

    Create GitHubCliCreatePullRequest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Worker/GitHubCliCreatePullRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\ComposerLocker\Worker;
+
+use Ghostwriter\ComposerLocker\Contract\Worker;
+use Ghostwriter\ComposerLocker\Event\Lock;
+use Ghostwriter\ComposerLocker\Process\ProcessRunner;
+use Ghostwriter\ComposerLocker\Value\GitHubCli;
+use Ghostwriter\ComposerLocker\Worker\Traits\WorkerTrait;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+final class GitHubCliCreatePullRequest implements Worker
+{
+    use WorkerTrait;
+
+    public function __construct(
+        private readonly ProcessRunner $processRunner,
+        private readonly SymfonyStyle $symfonyStyle,
+    ) {
+    }
+
+    public function work(Lock $lock): void
+    {
+        $this->symfonyStyle->success($this->processRunner->run(GitHubCli::CREATE_PULL_REQUEST));
+    }
+}
