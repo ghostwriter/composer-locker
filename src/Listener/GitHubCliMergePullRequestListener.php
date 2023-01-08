76fdadf3ea491b75e9882dda1e0978060aa57109
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-01-07 21:24:01 -0600

    Update GitHubCliMergePullRequestListener.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Listener/GitHubCliMergePullRequestListener.php
+++ src/Listener/GitHubCliMergePullRequestListener.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace Ghostwriter\ComposerLocker\Listener;
 
-use Ghostwriter\ComposerLocker\Event\GitHubCliCreatePullRequest;
 use Ghostwriter\ComposerLocker\Event\GitHubCliMergePullRequest;
 use Ghostwriter\ComposerLocker\Process\ProcessRunner;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 final class GitHubCliMergePullRequestListener
 {
-    private const COMMAND = ['echo','"gh pr merge"'];
+    private const COMMAND = ['echo', '"gh pr merge"'];
     // gh pr create --base "main" -f && gh pr merge --merge --delete-branch
 
     public function __construct(
