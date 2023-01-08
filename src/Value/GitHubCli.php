d1a77639f5546f3a54390c4624b915d4b9438aca
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-01-08 16:25:12 -0600

    Update GitHubCli.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Value/GitHubCli.php
+++ src/Value/GitHubCli.php
@@ -6,9 +6,28 @@ namespace Ghostwriter\ComposerLocker\Value;
 
 final class GitHubCli
 {
-    public const COMMIT = 'Bump composer dependencies';
+    public const BASE = Git::TARGET_BRANCH;
 
-    public const HEAD_BRANCH = Git::HEAD_BRANCH;
+    public const BODY = 'Bump composer dependencies' . PHP_EOL . PHP_EOL . self::SIGNATURE;
+
+    public const CREATE_PULL_REQUEST = [
+        'gh',
+        'pr',
+        'create',
+        '--head',
+        self::HEAD,
+        '--base',
+        self::BASE,
+        '--body',
+        self::BODY,
+        '--title',
+        self::TITLE,
+        '--fill',
+    ];
+
+    public const HEAD = Git::HEAD_BRANCH;
+
+    public const MERGE_PULL_REQUEST = ['gh', 'pr', 'merge', '--merge', '--delete-branch'];
 
     public const SIGNATURE = 'Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>';
 
