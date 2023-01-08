2e2bb34ae0dcd1ca3f77c9213c4ae1198dc93e91
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-01-08 16:44:50 -0600

    Update GitHubCli.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Value/GitHubCli.php
+++ src/Value/GitHubCli.php
@@ -6,30 +6,15 @@ namespace Ghostwriter\ComposerLocker\Value;
 
 final class GitHubCli
 {
-    public const BASE = Git::TARGET_BRANCH;
-
-    public const BODY = 'Bump composer dependencies' . PHP_EOL . PHP_EOL . self::SIGNATURE;
-
     public const CREATE_PULL_REQUEST = [
         'gh',
         'pr',
         'create',
         '--head',
-        self::HEAD,
+        Git::FEATURE_BRANCH,
         '--base',
-        self::BASE,
-        '--body',
-        self::BODY,
-        '--title',
-        self::TITLE,
+        Git::TARGET_BRANCH,
         '--fill',
     ];
-
-    public const HEAD = Git::HEAD_BRANCH;
-
     public const MERGE_PULL_REQUEST = ['gh', 'pr', 'merge', '--merge', '--delete-branch'];
-
-    public const SIGNATURE = 'Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>';
-
-    public const TITLE = 'Lock file maintenance';
 }
