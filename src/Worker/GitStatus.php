019539326d0a66faaa24b316957cd3826d1901b3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-03 12:23:08 -0500

    Update GitStatus.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Worker/GitStatus.php
+++ src/Worker/GitStatus.php
@@ -5,25 +5,15 @@ declare(strict_types=1);
 namespace Ghostwriter\ComposerLocker\Worker;
 
 use Ghostwriter\ComposerLocker\Contract\Worker;
-use Ghostwriter\ComposerLocker\Event\Lock;
-use Ghostwriter\ComposerLocker\Process\ProcessRunner;
+use Ghostwriter\ComposerLocker\Value\Git;
 use Ghostwriter\ComposerLocker\Worker\Traits\WorkerTrait;
-use Symfony\Component\Console\Style\SymfonyStyle;
 
 final class GitStatus implements Worker
 {
     use WorkerTrait;
 
-    private const COMMAND = ['git', 'status', '-s'];
-
-    public function __construct(
-        private readonly ProcessRunner $processRunner,
-        private readonly SymfonyStyle $symfonyStyle,
-    ) {
-    }
-
-    public function work(Lock $lock): void
+    public function command(): array
     {
-        $this->symfonyStyle->success($this->processRunner->run(self::COMMAND, $lock->getCurrentWorkingDirectory()));
+        return Git::STATUS;
     }
 }
