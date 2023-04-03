a1718bcb09978e59c0230fd14f4e6561546706b5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-03 12:23:00 -0500

    Update GitHubCliMergePullRequest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Worker/GitHubCliMergePullRequest.php
+++ src/Worker/GitHubCliMergePullRequest.php
@@ -5,26 +5,15 @@ declare(strict_types=1);
 namespace Ghostwriter\ComposerLocker\Worker;
 
 use Ghostwriter\ComposerLocker\Contract\Worker;
-use Ghostwriter\ComposerLocker\Event\Lock;
-use Ghostwriter\ComposerLocker\Process\ProcessRunner;
 use Ghostwriter\ComposerLocker\Value\GitHubCli;
 use Ghostwriter\ComposerLocker\Worker\Traits\WorkerTrait;
-use Symfony\Component\Console\Style\SymfonyStyle;
 
 final class GitHubCliMergePullRequest implements Worker
 {
     use WorkerTrait;
 
-    public function __construct(
-        private ProcessRunner $processRunner,
-        private SymfonyStyle $symfonyStyle,
-    ) {
-    }
-
-    public function work(Lock $lock): void
+    public function command(): array
     {
-        $this->symfonyStyle->success(
-            $this->processRunner->run(GitHubCli::MERGE_PULL_REQUEST, $lock->getCurrentWorkingDirectory())
-        );
+        return GitHubCli::MERGE_PULL_REQUEST;
     }
 }
